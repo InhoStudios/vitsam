78cb0d26a1026e1361794c139e490d851f966611
Andy Lizhe Zhao <ndsz@login02.sockeye.arc.ubc.ca>
2023-07-06 12:12:40 -0700

    Add promoted segmentation and onnx model

--- src/promoted_segmentation.py
+++ src/promoted_segmentation.py
@@ -4,6 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 from segment_anything import sam_model_registry, SamPredictor
 from segment_anything.utils.onnx import SamOnnxModel
+from os.path import join, exists
+import os
 
 import onnxruntime
 from onnxruntime.quantization import QuantType
@@ -15,7 +17,7 @@ def show_mask(mask, ax):
     mask_image = mask.reshape(h, w, 1) * colour.reshape(1, 1, -1)
     ax.imshow(mask_image)
 
-def show_points(coords, labels, ax, marker_size=375):
+def show_points(coords, labels, ax, marker_size=50):
     pos_points = coords[labels==1]
     neg_points = coords[labels==0]
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
@@ -26,8 +28,10 @@ def show_box(box, ax):
     w, h = box[2] - box[0], box[3] - box[1]
     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
 
-checkpoint = "../models/sam_model_best.pth"
+checkpoint = "../model/vitsam/sam_model_best.pth"
 model_type = "vit_b"
+output_dir = "/scratch/st-tklee-1/ndsz/promoted_w_onnx/"
+os.makedirs(output_dir, exist_ok=True)
 
 sam = sam_model_registry[model_type](checkpoint=checkpoint)
 
@@ -35,19 +39,94 @@ onnx_model_path = None
 
 import warnings
 
-onnx_model_path = "sam_onnx_example.onnx"
+onnx_model_path = join(output_dir, "sam_onnx_example.onnx")
 
-onnx_model = SamOnnxModel(sam, return_single_mask=True)
+if not exists(onnx_model_path):
+    onnx_model = SamOnnxModel(sam, return_single_mask=True)
 
-dynamic_axes = {
-    "point_coords": {1: "num_points"},
-    "point_labels": {1: "num_points"}
+    dynamic_axes = {
+        "point_coords": {1: "num_points"},
+        "point_labels": {1: "num_points"}
+    }
+
+    embed_dim = sam.prompt_encoder.embed_dim
+    embed_size = sam.prompt_encoder.image_embedding_size
+    mask_input_size = [4 * x for x in embed_size]
+    dummy_inputs = {
+        "image_embeddings": torch.randn(1, embed_dim, *embed_size, dtype=torch.float),
+        "point_coords": torch.randint(low=0, high=1024, size=(1, 5, 2), dtype=torch.float),
+        "point_labels": torch.randint(low=0, high=4, size=(1, 5), dtype=torch.float),
+        "mask_input": torch.randn(1, 1, *mask_input_size, dtype=torch.float),
+        "has_mask_input": torch.tensor([1], dtype=torch.float),
+        "orig_im_size": torch.tensor([1500, 2250], dtype=torch.float),
+    }
+    output_names = ["masks", "iou_predictions", "low_res_masks"]
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)
+        warnings.filterwarnings("ignore", category=UserWarning)
+        with open(onnx_model_path, "wb") as f:
+            torch.onnx.export(
+                onnx_model,
+                tuple(dummy_inputs.values()),
+                f, 
+                export_params=True,
+                verbose=False,
+                opset_version=17,
+                do_constant_folding=True,
+                input_names=list(dummy_inputs.keys()),
+                output_names=output_names,
+                dynamic_axes=dynamic_axes
+            )
+
+im_path = "../data/training_images/test/images/test3.jpg"
+
+image = cv2.imread(im_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image)
+plt.axis('on')
+# plt.show()
+plt.savefig(join(output_dir, "fig.jpg"))
+
+ort_session = onnxruntime.InferenceSession(onnx_model_path)
+
+sam.to(device="cuda")
+predictor = SamPredictor(sam)
+
+predictor.set_image(image)
+
+image_embedding = predictor.get_image_embedding().cpu().numpy()
+
+input_point = np.array([[500, 626], [329, 389], [306, 568], [248, 487], [197, 634], [385, 843], [440, 931], [457, 978], [331, 682], [639, 649],
+                        [540, 883], [284, 529], [348, 620], [357, 428], [390, 548]])
+input_label = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        0, 0, 0, 0, 0])
+
+onnx_coord = np.concatenate([input_point, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
+onnx_label = np.concatenate([input_label, np.array([-1])], axis=0)[None, :].astype(np.float32)
+
+onnx_coord = predictor.transform.apply_coords(onnx_coord, image.shape[:2]).astype(np.float32)
+
+onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
+onnx_has_mask_input = np.zeros(1, dtype=np.float32)
+
+ort_inputs = {
+    "image_embeddings": image_embedding,
+    "point_coords": onnx_coord,
+    "point_labels": onnx_label,
+    "mask_input": onnx_mask_input,
+    "has_mask_input": onnx_has_mask_input,
+    "orig_im_size": np.array(image.shape[:2], dtype=np.float32)
 }
 
-embed_dim = sam.prompt_encoder.embed_dim
-embed_size = sam.prompt_encoder.image_embedding_size
-mask_input_size = [4 * x for x in embed_size]
-dummy_inputs = {
-    "image_embeddings": torch.randn(1, embed_dim, *embed_size, dtype=torch.float),
-    
-}
+masks, _, low_res_logits = ort_session.run(None, ort_inputs)
+masks = masks > predictor.model.mask_threshold
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image)
+show_mask(masks, plt.gca())
+show_points(input_point, input_label, plt.gca())
+plt.axis('off')
+plt.savefig(join(output_dir, "promoted.jpg"))
